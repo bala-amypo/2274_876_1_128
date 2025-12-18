4f41c8f96718006e2eb681bbe4d6752f62e7c60e
bala-amypo <bala@amypo.in>
2025-12-18 00:19:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/InvestorProfile.java
+++ src/main/java/com/example/demo/entity/InvestorProfile.java
@@ -5,10 +5,29 @@ import jakarta.persistence.*;
 public class InvestorProfile{
     @Id;
     private Long id;
-    private String InvestorId;
+    private String investorId;
     private String fullName;
     private String email;
     private Boolean active;
     private LocalDateTime createdAt;
+    
 
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getInvestorId() {
+        return investorId;
+       }
+    public void setId(String investorId) {
+        this.investorId = investorId;
+    }
+    public String getFullName() {
+           return fullName;
+       }
+       public void setId(String fullName) {
+           this.fullName = fullName;
+       }
 }
