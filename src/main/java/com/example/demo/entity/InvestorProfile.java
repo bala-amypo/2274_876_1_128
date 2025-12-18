c84d542641461e1dac04ff77035af467e1f54b2d
bala-amypo <bala@amypo.in>
2025-12-18 00:14:37 +0000

    User commit

--- src/main/java/com/example/demo/entity/InvestorProfile.java
+++ src/main/java/com/example/demo/entity/InvestorProfile.java
@@ -1,11 +1,14 @@
 package com.example.demo.entity;
 import jakarta.persistence.*;
 
-@
+@Entity;
 public class InvestorProfile{
+    @Id;
     private Long id;
     private String InvestorId;
     private String fullName;
     private String email;
+    private Boolean active;
+    private LocalDateTime createdAt;
 
 }
