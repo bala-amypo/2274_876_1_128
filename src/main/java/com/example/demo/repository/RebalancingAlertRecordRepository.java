fc40fd7a815d646453e54e4e5962b342776dfe82
bala-amypo <bala@amypo.in>
2025-12-19 10:04:28 +0000

    User commit

--- src/main/java/com/example/demo/repository/RebalancingAlertRecordRepository.java
+++ src/main/java/com/example/demo/repository/RebalancingAlertRecordRepository.java
@@ -1,14 +1,17 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.RebalancingAlert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
-public interface RebalancingAlertRepository
-        extends JpaRepository<RebalancingAlert, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-    List<RebalancingAlert> findByInvestorId(Long investorId);
+import com.example.demo.entity.RebalancingAlertRecord;
 
-    List<RebalancingAlert> findByAlertTriggeredTrue();
+@Repository
+public interface RebalancingAlertRecordRepository
+        extends JpaRepository<RebalancingAlertRecord, Long> {
+
+    List<RebalancingAlertRecord> findByInvestorId(Long investorId);
+
+    List<RebalancingAlertRecord> findByResolvedFalse();
 }
