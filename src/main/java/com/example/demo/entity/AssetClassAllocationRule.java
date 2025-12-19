2ad67124f0b1bebdd88de72d9e955c73fa3b3db7
bala-amypo <bala@amypo.in>
2025-12-19 05:06:20 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/AssetClassAllocationRule.java
@@ -0,0 +1,53 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import com.example.demo.entity.enums.AssetClassType;
+
+@Entity
+public class AssetClassAllocationRule {
+
+    @Id
+   // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long investorId;
+
+    @Enumerated(EnumType.STRING)
+    private AssetClassType assetClass;
+
+    private Double targetPercentage;
+
+    private Boolean active;
+
+    
+    public AssetClassAllocationRule() {
+        
+    }
+
+  
+    public AssetClassAllocationRule(Long id, Long investorId,
+                                    AssetClassType assetClass,
+                                    Double targetPercentage, Boolean active) {
+        this.id = id;
+        this.investorId = investorId;
+        this.assetClass = assetClass;
+        this.targetPercentage = targetPercentage;
+        this.active = active;
+    }
+
+ 
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Long getInvestorId() { return investorId; }
+    public void setInvestorId(Long investorId) { this.investorId = investorId; }
+
+    public AssetClassType getAssetClass() { return assetClass; }
+    public void setAssetClass(AssetClassType assetClass) { this.assetClass = assetClass; }
+
+    public Double getTargetPercentage() { return targetPercentage; }
+    public void setTargetPercentage(Double targetPercentage) { this.targetPercentage = targetPercentage; }
+
+    public Boolean getActive() { return active; }
+    public void setActive(Boolean active) { this.active = active; }
+}
