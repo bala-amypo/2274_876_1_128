af54363d6ba67371431275eb167a272d52530dac
bala-amypo <bala@amypo.in>
2025-12-22 10:07:39 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/UserAccountService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import java.util.Optional;
+
+import com.example.demo.entity.UserAccount;
+
+public interface UserAccountService {
+
+    Optional<UserAccount> getUserDataFromDB(Long id);
+
+    UserAccount postUserDateToDB(UserAccount userAccount);
+
+    Optional<UserAccount> updateUserDataInDB(Long id, UserAccount userAccount);
+
+    String deleteUserDataInDB(Long id);
+}
---
+++ src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.demo.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.UserAccount;
+import com.example.demo.repository.UserAccountRepository;
+import com.example.demo.service.UserAccountService;
+
+@Service
+public class UserAccountServiceImpl implements UserAccountService {
+
+    private final UserAccountRepository userAccountRepository;
+
+    public UserAccountServiceImpl(UserAccountRepository userAccountRepository) {
+        this.userAccountRepository = userAccountRepository;
+    }
+
+    @Override
+    public Optional<UserAccount> getUserDataFromDB(Long id) {
+        return userAccountRepository.findById(id);
+    }
+
+    @Override
+    public UserAccount postUserDateToDB(UserAccount userAccount) {
+        return userAccountRepository.saveAndFlush(userAccount);
+    }
+
+    @Override
+    public Optional<UserAccount> updateUserDataInDB(Long id, UserAccount userAccount) {
+        userAccountRepository.deleteById(id);
+        userAccountRepository.saveAndFlush(userAccount);
+        return userAccountRepository.findById(id);
+    }
+
+    @Override
+    public String deleteUserDataInDB(Long id) {
+        userAccountRepository.deleteById(id);
+        return "Deleted Successfully";
+    }
+}
